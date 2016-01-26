1ce03be2b63ef2ecc3688ffb4e4b7d2f0670a12c
Nikita Nikolaevsky <nikiror@gmail.com>
2016-01-26 19:45:06 +0800

    Edit format

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-	let!(:question) { create(:question) }
-	let!(:answer) { create(:answer, question: question) }
+  let!(:question) { create(:question) }
+  let!(:answer) { create(:answer, question: question) }
 
-	describe 'POST #create' do
-		context 'with valid attributes' do
-	  	it 'save the new answer in database' do
-				expect { post :create, id: answer, question_id: question, answer: attributes_for(:answer) }.to change(question.answers, :count).by(1)
-			end
-			it 'assign @answer to question' do
-				post :create, id: answer, question_id: question, answer: attributes_for(:answer)
-				expect(assigns(:answer).question_id).to eq answer.question_id	
-			end
-			it 'render show create' do
-				post :create, id: answer, question_id: question, answer: attributes_for(:answer)
-				expect(response).to render_template :create	
-			end
-	 end
+  describe 'POST #create' do
+   context 'with valid attributes' do
+      it 'save the new answer in database' do
+        expect { post :create, id: answer, question_id: question, answer: attributes_for(:answer) }.to change(question.answers, :count).by(1)
+      end
+      it 'assign @answer to question' do
+        post :create, id: answer, question_id: question, answer: attributes_for(:answer)
+        expect(assigns(:answer).question_id).to eq answer.question_id 
+      end
+      it 'render show create' do
+        post :create, id: answer, question_id: question, answer: attributes_for(:answer)
+        expect(response).to render_template :create 
+      end
+   end
 
-	 context 'with invalid attributes' do
-	  	it 'dont save the new answer in database' do
-				expect { post :create, id: answer, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(question.answers, :count)
-			end
-			it 're-render new create' do
-				post :create, id: answer, question_id: question, answer: attributes_for(:invalid_answer)
-				expect(response).to render_template :create	
-			end
-	 end
-	end
+   context 'with invalid attributes' do
+      it 'dont save the new answer in database' do
+        expect { post :create, id: answer, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(question.answers, :count)
+      end
+      it 're-render new create' do
+        post :create, id: answer, question_id: question, answer: attributes_for(:invalid_answer)
+        expect(response).to render_template :create 
+      end
+   end
+  end
 
-	describe 'PATCH #update' do
-		context 'with valid attributes' do
-			it 'assigns requsted answer to @answer' do
-				patch :update, id: answer, question_id: question, answer: attributes_for(:answer)
-				expect(assigns(:answer)).to eq answer
-			end
-			it 'update answer in database' do
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns requsted answer to @answer' do
+        patch :update, id: answer, question_id: question, answer: attributes_for(:answer)
+        expect(assigns(:answer)).to eq answer
+      end
+      it 'update answer in database' do
         patch :update,  id: answer, question_id: question, answer: { content: 'new content' }
         answer.reload
         expect(answer.content).to eq 'new content'
       end
       it 'redirects to updated answer' do
-				patch :update,  id: answer, question_id: question, answer: attributes_for(:answer)
-				expect(response).to redirect_to question
-			end
-		end
+        patch :update,  id: answer, question_id: question, answer: attributes_for(:answer)
+        expect(response).to redirect_to question
+      end
+    end
 
     context 'with invalid params' do
       it 'dont update answer in database' do
@@ -53,10 +53,10 @@ RSpec.describe AnswersController, type: :controller do
         answer.reload
         expect(answer.content).to eq answer.content
       end
-		end
-	end
+    end
+  end
 
-	describe 'DELETE #destroy' do
+  describe 'DELETE #destroy' do
     it 'should delete answer from database' do
       expect { delete :destroy, id: answer, question_id: question }.to change(question.answers, :count).by(-1)
     end
